33da1875f073caaa4eb8a6b68cc4e7de82770c71
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-01 21:37:41 +0100

    Add counter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script>
+	let num = 0;
+
+	function addOne() {
+		num++;
+	}
+</script>
 <h1>Welcome to SvelteKit</h1>
+<p>{num}</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button type="button" on:click={addOne}>Add 1</button>
