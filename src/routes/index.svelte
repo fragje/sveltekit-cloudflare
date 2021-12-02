3281c55b0e455360e583f696393fe37370e9eae4
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-02 08:26:48 +0100

    Add new button 'add 2'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,13 @@
 	function addOne() {
 		num++;
 	}
+
+	function addTwo() {
+		num = num + 2;
+	}
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>{num}</p>
 <button type="button" on:click={addOne}>Add 1</button>
+<button type="button" on:click={addTwo}>Add 2</button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
