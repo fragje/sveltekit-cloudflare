588fe858bab6c0bb6601080f1db40c45ad34f7ea
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-02 08:18:02 +0100

    Reposition add button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,5 +7,5 @@
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>{num}</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button type="button" on:click={addOne}>Add 1</button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
