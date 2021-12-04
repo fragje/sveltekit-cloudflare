f7c6b0fde45f9c4ded3880682b2985898625664b
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-04 21:07:38 +0100

    Update location

--- functions/location.js
+++ functions/location.js
@@ -4,7 +4,8 @@ export async function onRequest(context) {
   const value = await context.env.TEST.get("click");
   console.log(value);
 
-  console.log(context.env);
-  const {cf} = context.request;
-  return new Response(JSON.stringify(cf, null, 2), { 'Content-Type': 'application/json' });
+  // console.log(context.env);
+  // const {cf} = context.request;
+  // return new Response(JSON.stringify(cf, null, 2), { 'Content-Type': 'application/json' });
+  return new Response(value)
 }
