c655c6e97af3452edbe318266ade66dae21a47b1
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-04 19:45:36 +0100

    Update location and wrangler

--- functions/location.js
+++ functions/location.js
@@ -1,6 +1,10 @@
 // Reacts to POST /hello-world
 export async function onRequest(context) {
   // ...
+  const value = await context.env.TEST.get("click");
+  console.log(value);
+
+  console.log(context.env);
   const {cf} = context.request;
   return new Response(JSON.stringify(cf, null, 2), { 'Content-Type': 'application/json' });
 }
