02b11c17ac22aaef4cb1c5961057e2d689183b27
Frank Gjertsen <frank.gjertsen@gmail.com>
2021-12-05 08:40:54 +0100

    Change namespace to KV

--- functions/location.js
+++ functions/location.js
@@ -1,11 +1,13 @@
 // Reacts to POST /hello-world
-export async function onRequest(context) {
+export async function onRequest({env}) {
   // ...
-  const value = await context.env.TEST.get("click");
-  console.log(value);
+  const value = await env.KV.get('click');
+  // const value = test.TEST.get("click");
+  // const value2 = context.env.TEST.get("click");
+  console.log({env, value});
 
   // console.log(context.env);
   // const {cf} = context.request;
   // return new Response(JSON.stringify(cf, null, 2), { 'Content-Type': 'application/json' });
-  return new Response(value)
+  return new Response('hello', value)
 }
